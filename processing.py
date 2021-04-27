9842d587df3638c442b1b497eb06811e3872b4a7
wen-haoran <haoranwen1@gmail.com>
2021-04-26 22:01:09 -0400

    edited processing

--- processing.py
+++ processing.py
@@ -1,22 +1,31 @@
 import cv2
-# Read the image
-img = cv2.imread('imgs/original.jpg')
-print(img.shape)
-height = img.shape[0]
-width = img.shape[1]
 
-# Cut the image in half
-width_cutoff = width // 2
-left = img[:, :width_cutoff]
-right = img[:, width_cutoff:]
+def init_processing():
+    # Read the image
+    img = cv2.imread('imgs/original.jpg')
+    print(img.shape)
+    height = img.shape[0]
+    width = img.shape[1]
 
-cv2.imwrite("./imgs/left.jpg", left)
-cv2.imwrite("./imgs/right.jpg", right)
+    # Cut the image in half
+    width_cutoff = width // 2
+    left = img[:, :width_cutoff]
+    right = img[:, width_cutoff:]
+
+    cv2.imwrite("./imgs/left.jpg", left)
+    cv2.imwrite("./imgs/right.jpg", right)
 
 
-#gray scale
-grayLeft = cv2.cvtColor(left, cv2.COLOR_BGR2GRAY)
-grayRight = cv2.cvtColor(right, cv2.COLOR_BGR2GRAY)
+    #gray scale
+    grayLeft = cv2.cvtColor(left, cv2.COLOR_BGR2GRAY)
+    grayRight = cv2.cvtColor(right, cv2.COLOR_BGR2GRAY)
 
-cv2.imwrite("./imgs/grayLeft.jpg", grayLeft)
-cv2.imwrite("./imgs/grayRight.jpg", grayRight)
+    cv2.imwrite("./imgs/grayLeft.jpg", grayLeft)
+    cv2.imwrite("./imgs/grayRight.jpg", grayRight)
+
+
+def main():
+    init_processing()
+
+if __name__ == "__main__":
+    main()
