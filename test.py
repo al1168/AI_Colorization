908848bb3f31123063599b70719b1531584d60bd
Alden Lu <al1168@scarletmail.rutgers.edu>
2021-04-30 01:00:33 -0400

    basic agent done?

--- test.py
+++ test.py
@@ -1,9 +1,18 @@
 # patch : 3 x 3 patch where the center pixel is the pixel we want to predict
 # representative_colors: 5 colors that represents all the color in the original image
 # grey_image: original image in a greyscaled
+import time
+from collections import Counter
+from queue import PriorityQueue
+import heapq
 import numpy as np
 import sys
-
+import cv2
+import matplotlib.pyplot as plt
+import statistics
+from statistics import mode
+from scipy import stats
+import random
 
 def eclud_dist(centroid, data_pnt):
     return np.linalg.norm(centroid - data_pnt)
@@ -12,10 +21,12 @@ def eclud_dist(centroid, data_pnt):
 def predictpixel(representative_colors, grey_image):
     pass
 
+
 def kmeans(input):
     colors = []
     id = list(range(0, len(input)))
     datapnts = {k: v for (k, v) in zip(id, input)}
+
     cluster_dict = {}
     for x in range(1, 6):
         r = np.random.randint(255)
@@ -29,9 +40,11 @@ def kmeans(input):
     # while the current and previous dont equal each other, just keep averaging the data pnts related to cluster
     '''for each data pnt, assign it to a centroid, then when they are assigned, calculate average for each cluster, 
     and set as new centroid, check for convergence repeat'''
-
+    final_clust_data = {}
     while not convergence(cluster_dict):
+        # stores which datapnt belong to which cluster
         clust_data = {}
+
         for x in range(1, 6):
             clust_data[x] = []
         for datapnts_id in datapnts.keys():
@@ -56,7 +69,7 @@ def kmeans(input):
             a = tmplst
             if len(tmplst) > 0:
                 average = np.mean(a, axis=0)
-                print(' Average of:' + str(a) + 'is ' + str(average))
+                # print(' Average of:' + str(a) + 'is ' + str(average))
                 previous = cluster_dict[key][0]
                 cluster_dict[key][0] = average
                 cluster_dict[key][1] = previous
@@ -64,10 +77,11 @@ def kmeans(input):
                 previous = cluster_dict[key][0]
                 cluster_dict[key][0] = np.array([-1, -1, -1])
                 cluster_dict[key][1] = previous
+        final_cluster_data = clust_data
     for key in cluster_dict.keys():
         colors.append(cluster_dict[key][0])
     print("finish" + str(len(colors)))
-    return colors
+    return colors, final_cluster_data
 
     # while not np.array_equal(previous,current):
 
@@ -79,14 +93,269 @@ def convergence(cluster_dict):
     return True
 
 
+# def main():
+#     img = cv2.imread('imgs/left.jpg')
+#     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#     # img[0:150, 0:300] = [0, 255, 0]
+#
+#     cluster_input = img.reshape((img.shape[1] * img.shape[0], 3))
+#     colors, cluster_dict = kmeans(cluster_input)
+#
+#     h = img.shape[0]
+#     w = img.shape[1]
+#     for y in range(0, h):
+#         for x in range(0, w):
+#             min = sys.maxsize
+#             tmpcolor = [0, 0, 0]
+#             for color in colors:
+#                 dist = eclud_dist(img[y, x], color)
+#                 if min > dist:
+#                     min = dist
+#                     tmpcolor = color
+#             img[y, x] = tmpcolor
+#
+#     plt.imshow(img)
+#     plt.show()
+#     # ///////////////////////////////////////////////////////////////////////
+#     # recolor the right side of the image
+#     '''
+#     We analyze the greyscale of both the right and left, for each pixel on right side, take a patch surrounding
+#     that pixel and find 6 patches on the left side most similar to that pixel
+#     '''
+#     greyR = cv2.imread('imgs/grayRight.jpg')
+#     greyL = cv2.imread('imgs/grayLeft.jpg')
+#     greydict = {}
+#     h = greyL.shape[0]
+#     w = greyL.shape[1]
+#     # assign neighbors for all the leftside
+#     for i in range(2, h - 1):
+#         for j in range(2, w - 1, 3):
+#             tmplst = [greyL[i + 1, j],
+#                       greyL[i - 1, j],
+#                       greyL[i, j + 1],
+#                       greyL[i, j - 1],
+#                       greyL[i - 1, j - 1],
+#                       greyL[i + 1, j - 1],
+#                       greyL[i - 1, j + 1],
+#                       greyL[i + 1, j + 1],
+#                       greyL[i, j]]
+#             flat_list = np.array(np.concatenate(tmplst).flat)
+#             greydict[(i, j)] = flat_list
+#     o = greyR.shape[0]
+#     p = greyR.shape[1]
+#     # compute each pixel and predict color
+#     for i in range(2, o - 1):
+#         for j in range(2, p - 1):
+#             print(i)
+#             tmplst = [greyR[i + 1, j],
+#                       greyR[i - 1, j],
+#                       greyR[i, j + 1],
+#                       greyR[i, j - 1],
+#                       greyR[i - 1, j - 1],
+#                       greyR[i + 1, j - 1],
+#                       greyR[i - 1, j + 1],
+#                       greyR[i + 1, j + 1],
+#                       greyR[i, j]]
+#             flat_list = np.array(np.concatenate(tmplst).flat)
+#             pqe = PriorityQueue()
+#             for key in greydict.keys():
+#                 dist = eclud_dist(flat_list, greydict[key])
+#                 pqe.put((dist, key))
+#             clst = []
+#             nclst = []
+#             priolst = []
+#             for x in range(6):
+#                 patch = pqe.get()
+#                 q, z = patch[1]
+#                 color = img[q, z]
+#                 clst.append(color)
+#                 print(color)
+#                 print(clst)
+#             m = stats.mode(clst)
+#             mode1 = m[0]
+#             for x in range(len(clst)):
+#                 comparison = clst[x] == m[0]
+#                 equal_arrays = comparison.all()
+#                 # print(equal_arrays)
+#                 if not equal_arrays:
+#                     clst.append(clst[x])
+#             y = stats.mode(nclst)
+#             mode2 = y[0]
+#             if y[1] == m[1]:
+#                 for x in range(len(clst)):
+#                     popped = clst.pop()
+#                     if popped == mode1:
+#                         greyR[i, j] = mode2
+#                     if popped == mode2:
+#                         greyR[i, j] = mode1
+#                         break
+#             else:
+#                 greyR[i, j] = mode1
+#
+#     plt.imshow(greyR)
+#     plt.show()
+#     lst = []
+#     # lst = [np.array([0,0,0]),np.array([255,255,255]),np.array([51,51,51]),np.array([51,51,51]),np.array([51,51,51]),np.array([51,51,51]),np.array([102,102,102]),np.array([153,153,153]),np.array([0,0,153]),np.array([0,0,153]),np.array([0,0,153])]
+#     # m = stats.mode(lst)
+#     # flat_list = np.array(np.concatenate(m[0]).flat)
+#     # # print(flat_list)
+#     # nlst = []
+#     # # for arr in lst:
+#     # #     print(arr)
+#     # #     comparison = arr == m[0]
+#     # #     # print(comparison)
+#     # #     equal_arrays = comparison.all()
+#     # #     print(equal_arrays)
+#     # #     # if not equal_arrays:
+#     # #     #     lst.append(arr)
+#     # for x in range(len(lst)):
+#     #     comparison = lst[x] == m[0]
+#     #     equal_arrays = comparison.all()
+#     #     # print(equal_arrays)
+#     #     if not equal_arrays:
+#     #         nlst.append(lst[x])
+#     # y = stats.mode(nlst)
+#
+#     # for x in range(10):
+#     #     lst.append(np.random.randint(low=0, high=255, size=3))
+#     # print(lst)
+#     # print(kmeans(lst))
+
+def get_patches(img):
+    patches=[]
+    #iterate through grayleft
+    #iterate through rows
+    for i in range(1,len(img)-1):
+        #iterate through columns
+        for j in range(1,len(img[0])-1):
+            #grayleft[i][j] starts on middle pixel
+            #find the rest of the patch (adjacent pixels)
+            patches.append((img[i-1:i+2,j-1:j+2],(i,j)))
+
+    return patches
 def main():
-    lst = []
-    # lst = [np.array([0,0,0]),np.array([255,255,255]),np.array([51,51,51]),np.array([102,102,102]),np.array([153,153,153])]
-    for x in range(10):
-        lst.append(np.random.randint(low=0, high=255, size=3))
-    print(lst)
-    print(kmeans(lst))
+    img = cv2.imread('imgs/left.jpg')
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # img[0:150, 0:300] = [0, 255, 0]
 
+    cluster_input = img.reshape((img.shape[1] * img.shape[0], 3))
+    colors, cluster_dict = kmeans(cluster_input)
 
+    h = img.shape[0]
+    w = img.shape[1]
+    for y in range(0, h):
+        for x in range(0, w):
+            min = sys.maxsize
+            tmpcolor = [0, 0, 0]
+            for color in colors:
+                dist = eclud_dist(img[y, x], color)
+                if min > dist:
+                    min = dist
+                    tmpcolor = color
+            img[y, x] = tmpcolor
+
+    plt.imshow(img)
+    plt.show()
+    # ///////////////////////////////////////////////////////////////////////
+    # recolor the right side of the image
+    '''
+    We analyze the greyscale of both the right and left, for each pixel on right side, take a patch surrounding
+    that pixel and find 6 patches on the left side most similar to that pixel
+    '''
+    greyR = cv2.imread('imgs/grayRight.jpg')
+    greyL = cv2.imread('imgs/grayLeft.jpg')
+
+    greyLp = get_patches(greyL)
+
+    for i in range(1, len(greyR) - 1):
+        # iterate through columns
+        for j in range(1, len(greyR[0]) - 1):
+            print(i)
+            patch = greyR[i - 1:i + 2, j - 1:j + 2]
+            samples = random.sample(list(greyLp), 1000)
+            pqe = PriorityQueue()
+            for sample in samples:
+                dist = eclud_dist(sample[0], greyR[i - 1:i + 2, j - 1:j + 2])
+                pqe.put((dist,sample[1]))
+            closestNeighbor = []
+            for x in range(6):
+                popped = pqe.get()
+                closestNeighbor.append(popped)
+            # print(closestNeighbor)
+            cl = []
+            for patch in closestNeighbor:
+                x, y = patch[1]
+                color = img[x,y]
+                cl.append(color)
+            mode1 = stats.mode(cl)
+            mode1color = mode1[0]
+            print("mode1 =" + str(mode1))
+            nlst = []
+            for patch in closestNeighbor:
+                x, y = patch[1]
+                color = img[x, y]
+                comparison = color == mode1color
+                equal_arrays = comparison.all()
+                if not equal_arrays:
+                    nlst.append(color)
+            mode2 = stats.mode(nlst)
+            mode2color = mode2[0]
+            print("cnt of mode1"+ str(mode1[1])+"\tcnt of mode2:" +str(mode2[1]))
+            if len(mode2[1]) > 0:
+                cnt1 = np.array(np.concatenate(mode1[1]).flat)
+                cnt2 = np.array(np.concatenate(mode2[1]).flat)
+
+                comp = cnt1 == cnt2
+                e = comp.all()
+                print(e)
+                if e:
+                    for ptr in closestNeighbor:
+                        popped = closestNeighbor.pop()
+                        if popped == mode1color:
+                            greyR[i,j] = mode2color
+                            break
+                        if popped == mode2color:
+                            greyR[i,j] = mode1color
+            else:
+                greyR[i,j] = mode1color
+    plt.imshow(greyR)
+    plt.show()
+            #             for x in range(6):
+            #                 patch = pqe.get()
+            #                 q, z = patch[1]
+            #                 color = img[q, z]
+            #                 clst.append(color)
+            #                 print(color)
+            #                 print(clst)
+            #             m = stats.mode(clst)
+            #             mode1 = m[0]
+            #             for x in range(len(clst)):
+            #                 comparison = clst[x] == m[0]
+            #                 equal_arrays = comparison.all()
+            #                 # print(equal_arrays)
+            #                 if not equal_arrays:
+            #                     clst.append(clst[x])
+            #             y = stats.mode(nclst)
+            #             mode2 = y[0]
+            #             if y[1] == m[1]:
+            #                 for x in range(len(clst)):
+            #                     popped = clst.pop()
+            #                     if popped == mode1:
+            #                         greyR[i, j] = mode2
+            #                     if popped == mode2:
+            #                         greyR[i, j] = mode1
+            #                         break
+            #             else:
+            #                 greyR[i, j] = mode1
+
+                # for x in range(len(clst)):
+                #                 popped = clst.pop()
+                #                 if popped == mode1:
+                #                     greyR[i, j] = mode2
+                #                 if popped == mode2:
+                #                     greyR[i, j] = mode1
+                #                     break
+                #         else:
+                #             greyR[i, j] = mode1
 if __name__ == "__main__":
     main()
